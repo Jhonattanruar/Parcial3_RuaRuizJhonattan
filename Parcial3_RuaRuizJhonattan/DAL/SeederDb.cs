12830f5d079da3866eb0b42fcdb0cbfbbf7ea4c9
JHONATTAN ALBERTO RUA RUIZ <jhonattanrua258527@correo.itm.edu.co>
2023-11-12 08:07:19 -0500

    Created SeederDB

---
+++ Parcial3_RuaRuizJhonattan/DAL/SeederDb.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+
+namespace Parcial3_RuaRuizJhonattan.DAL
+{
+    public class SeederDb
+    {
+        private readonly DataBaseContext _context;
+
+        public SeederDb(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync() {
+
+            await _context.Database.EnsureCreatedAsync();
+
+            await PopulateTicketsAsync();
+        }
+
+        #region Private Methos
+
+        private async Task PopulateTicketsAsync()
+        {
+
+            if (!_context.Tickets.Any())
+            {
+                for(int i = 0; i < 1000; i++)
+                {
+                    _context.Tickets.Add(new Entities.Tickets
+                    {
+                        UseDate = 'null',
+                        IsUsed = false,
+                        EntranceGate = 'null'
+                    });
+                }
+                
+
+
+
+        }
+
+    }
+
+    #endregion
+
+}
+}
